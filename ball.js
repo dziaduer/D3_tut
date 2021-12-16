04893c553a660805e97d7b1726bb7cff6df2205a
dziaduer <dziaduer@gmail.com>
2021-12-16 17:07:24 +0100

    Added ball, simple ball movement logic

--- ball.js
+++ ball.js
@@ -1,13 +1,36 @@
-let ball = xPos = 500, yPos = 500, radius = 100;
 
-function updateBall(){
-    field.select('g')
-        .enter().append('circle')
-        .data(ball)
+let balls = [], xPos = 1000, yPos = 1000, radius = 100;
+
+function newBall() {
+	balls = [];
+    balls.push({
+        id: 0,
+		x: 100,
+		y: 100
+    });
+}
+
+function updateBall() {
+	field.select('g')
+        .append('circle')
+		.data(balls)
+		.join('circle')
         .attr('fill', '#000000')
 		.attr('cx', xPos)
 		.attr('cy', yPos)
-		.attr('r', radius);
+		.attr('r', radius)
+        .attr('id', 'ball_0');
 }
 
+function moveBall(){
+    let xVal = +document.getElementById('x-input').value;
+    let yVal = +document.getElementById('y-input').value;
+    field.select('g')
+        .selectAll('#ball_0')
+        .attr('fill', '#13c2c1')
+        .transition()
+        .attr('transform', 'translate(' + xVal + ', ' + yVal + ')').duration(1000);
+}
+
+newBall();
 updateBall();
